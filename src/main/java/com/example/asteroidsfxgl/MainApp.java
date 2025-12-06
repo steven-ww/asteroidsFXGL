c25963610b805b61bd0ad75afa0836b9048f16e7
Steven Webber <webber.steven@gmail.com>
2025-12-06 16:50:05 +0200

    movement

---
+++ src/main/java/com/example/asteroidsfxgl/MainApp.java
@@ -0,0 +1,93 @@
+package com.example.asteroidsfxgl;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import java.util.Map;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class MainApp extends GameApplication {
+
+  private Entity player;
+
+  @Override
+  protected void initSettings(GameSettings settings) {
+    settings.setTitle("Astroids FXGL");
+    settings.setWidth(1280);
+    settings.setHeight(720);
+  }
+
+  @Override
+  protected void initUI() {
+    Text textPixels = new Text();
+    textPixels.setTranslateX(50); // x = 50
+    textPixels.setTranslateY(100); // y = 100
+    textPixels.textProperty().bind(FXGL.getWorldProperties().intProperty("pixelsMoved").asString());
+
+    FXGL.getGameScene().addUINode(textPixels); // add to the scene graph
+    //    FXGL.getGameScene().setBackgroundColor(Color.BLACK);
+  }
+
+  @Override
+  protected void initGame() {
+    player =
+        FXGL.entityBuilder().at(300, 300).view(new Rectangle(25, 25, Color.BLUE)).buildAndAttach();
+  }
+
+  @Override
+  protected void initInput() {
+    Input input = FXGL.getInput();
+
+    input.addAction(
+        new UserAction("Move Right") {
+          @Override
+          protected void onAction() {
+            player.translateX(5); // move right 5 pixels
+            FXGL.inc("pixelsMoved", +5);
+          }
+        },
+        KeyCode.D);
+
+    input.addAction(
+        new UserAction("Move Left") {
+          @Override
+          protected void onAction() {
+            player.translateX(-5); // move left 5 pixels
+          }
+        },
+        KeyCode.A);
+
+    input.addAction(
+        new UserAction("Move Down") {
+          @Override
+          protected void onAction() {
+            player.translateY(5); // move down 5 pixels
+          }
+        },
+        KeyCode.S);
+
+    input.addAction(
+        new UserAction("Move Up") {
+          @Override
+          protected void onAction() {
+            player.translateY(-5); // move up 5 pixels
+          }
+        },
+        KeyCode.W);
+  }
+
+  @Override
+  protected void initGameVars(Map<String, Object> vars) {
+    vars.put("pixelsMoved", 0);
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
