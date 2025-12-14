ecff574a1c24a2b762efaf60fb19a016728db6f4
noahs <noahscott.webber@gmail.com>
2025-12-14 11:16:54 +0200

    made the ship a triangle and made it rotate

--- src/main/java/com/example/asteroidsfxgl/MainApp.java
+++ src/main/java/com/example/asteroidsfxgl/MainApp.java
@@ -4,11 +4,13 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
 import java.util.Map;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
@@ -36,8 +38,19 @@ public class MainApp extends GameApplication {
 
   @Override
   protected void initGame() {
-    player =
-        FXGL.entityBuilder().at(300, 300).view(new Rectangle(25, 25, Color.BLUE)).buildAndAttach();
+//    player =
+//        FXGL.entityBuilder().at(300, 300).view(new Rectangle(25, 25, Color.BLUE)).buildAndAttach();
+
+      player = FXGL.entityBuilder()
+              .at(640, 360)
+              .view(new Polygon(
+                      0.0, -10.0,   // top point
+                      7.5, 10.0,    // bottom right
+                      -7.5, 10.0    // bottom left
+              ))
+              .with(new CollidableComponent(true))
+              .buildAndAttach();
+
   }
 
   @Override
@@ -59,15 +72,40 @@ public class MainApp extends GameApplication {
           @Override
           protected void onAction() {
             player.translateX(-5); // move left 5 pixels
+              FXGL.inc("pixelsMoved", +5);
+
           }
         },
         KeyCode.A);
+
+      input.addAction(
+              new UserAction("Turn Left") {
+                  @Override
+                  protected void onAction() {
+                      player.rotateBy(2.5); // move left 5 pixels
+                      FXGL.inc("pixelsMoved", +5);
+
+                  }
+              },
+              KeyCode.E);
+
+      input.addAction(
+              new UserAction("Turn Right") {
+                  @Override
+                  protected void onAction() {
+                      player.rotateBy(-2.5); // move left 5 pixels
+                      FXGL.inc("pixelsMoved", +5);
+
+                  }
+              },
+              KeyCode.Q);
 
     input.addAction(
         new UserAction("Move Down") {
           @Override
           protected void onAction() {
-            player.translateY(5); // move down 5 pixels
+            player.translateY(5);// move down 5 pixels
+              FXGL.inc("pixelsMoved", +5);
           }
         },
         KeyCode.S);
@@ -77,6 +115,7 @@ public class MainApp extends GameApplication {
           @Override
           protected void onAction() {
             player.translateY(-5); // move up 5 pixels
+              FXGL.inc("pixelsMoved", +5);
           }
         },
         KeyCode.W);
